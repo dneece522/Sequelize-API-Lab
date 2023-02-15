f20854d516662efe2898a8d314b247b2471635a7
Drew Neece <dneece5@aggienetwork.com>
2023-02-15 17:05:57 -0600

    add create animal functionality

--- controllers/animals.js
+++ controllers/animals.js
@@ -1,7 +1,14 @@
 const { Animal } = require('../models')
 
-
+const create = async (req, res) => {
+  try {
+    const animal = await Animal.create(req.body)
+    res.status(200).json(animal)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
 
 module.exports = {
-  
+  create,
 }
--- routes/animals.js
+++ routes/animals.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const animalsCtrl = require('../controllers/animals.js')
 
-
+router.post('/', animalsCtrl.create)
 
 module.exports = router
